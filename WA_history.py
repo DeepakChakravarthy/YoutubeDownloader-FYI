046a78efd11d8cb30c647d35c969e9b20754a428
Ramkumar <ramkumar.mrj97@gmail.com>
2020-06-19 15:11:49 +0530

    AddOn.py & WA_history.py file modified

--- WA_history.py
+++ WA_history.py
@@ -1,13 +1,16 @@
 import tkinter as tk
-from tkinter import Label
+from tkinter import Label, messagebox
 
 root = tk.Tk()
 root.title("WhatsApp History")
-root.iconbitmap("Assest/YoutubeDownloader.ico")
+root.iconbitmap("Assets/YoutubeDownloader.ico")
 root.geometry("200x400+1040+100")
 root.resizable(0,0)
-
-with open("pywhatkit_history.txt", "r") as f:
-    lb = Label(root, text=f.read()).pack()
+try:
+    with open("pywhatkit_history.txt", "r") as f:
+        Label(root, text=f.read()).pack()
+except FileNotFoundError:
+	messagebox.showinfo("WhatsApp History.",
+                     "No History found.")
 
 root.mainloop()
